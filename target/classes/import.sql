c950bddd2d5d66d16fb0ed2e6b003b0d8714cef6
Danielda <61012696+BoolDan@users.noreply.github.com>
2025-03-10 21:32:30 +0100

    Final changes V1

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,13 +1,10 @@
 -- insert admin (username a, password aa)
-INSERT INTO IWUser (id, enabled, roles, username, password)
+INSERT INTO Usuario (id, enabled, roles, username, password)
 VALUES (1, TRUE, 'ADMIN,USER', 'a',
     '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
-INSERT INTO IWUser (id, enabled, roles, username, password)
+INSERT INTO Usuario (id, enabled, roles, username, password)
 VALUES (2, TRUE, 'USER', 'b',
     '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
-
--- start id numbering from a value that is larger than any assigned above
-ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
 
 -- Insertar un torneo en estado "En espera" sin fecha de finalización
 INSERT INTO Torneo (estado, numero_participantes, fecha_inicio, fecha_fin)
@@ -28,3 +25,8 @@ VALUES ('Cerrado', 6, '2025-03-09 15:45:00', '2025-03-09 17:30:00');
 -- Insertar un torneo en estado "En espera" con pocos participantes
 INSERT INTO Torneo (estado, numero_participantes, fecha_inicio, fecha_fin)
 VALUES ('En espera', 4, '2025-03-10 18:00:00', NULL);
+
+-- start id numbering from a value that is larger than any assigned above
+ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
+
+
