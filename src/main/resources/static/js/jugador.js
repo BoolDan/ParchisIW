ae0b2806b44f6dae7d9ad8190bb29b27f3e9f7e1
Mauricio-NCB <mncalderon2001@gmail.com>
2025-04-08 10:38:01 +0200

    Revert "Updated game"
    This reverts commit e99b6ba4c401b0fcf75d0d2fcfbe43a44abbf350.

--- src/main/resources/static/js/jugador.js
+++ src/main/resources/static/js/jugador.js
@@ -1,47 +1,19 @@
-import { Fichas } from './ficha.js';
-
 class Jugador {
-
-
-    constructor(nombre, color, tipo = "Normal", fichas = []) {
+    constructor(nombre, color, tipo = "Normal") {
         this.nombre = nombre;
         this.color = color;
         this.tipo = tipo; // Normal o IA
         this.fichas = [];
     }
 
-    esperarClickFicha() {
-        return new Promise((resolve) => {
-            const fichasElementos = document.querySelectorAll(`.ficha[data-jugador="${this.color}"]`);
-            const manejador = (event) => {
-                fichasElementos.forEach(ficha => ficha.removeEventListener('click', manejador));
-                const fichaId = event.target.dataset.id; // Suponiendo que cada ficha tiene un atributo `data-id`
-                const fichaSeleccionada = this.fichas.find(ficha => ficha.id === fichaId);
-                resolve(fichaSeleccionada);
-            };
-    
-            fichasElementos.forEach(ficha => ficha.addEventListener('click', manejador));
-        });
-    }
-
-    async moverFicha(dado) {
-        //Buscamos primero si hay alguna ficha que se encuentre en la reserva d fichas del jugador (casa)
-        const encasa = this.fichas.find(ficha => ficha.encasa);
-        if (dado == 5 && encasa){
-            for (ficha in this.fichas){
-                if (ficha.encasa == true){
-                    ficha.sacar()
-                }
-            }
+    moverFicha(idFicha, dado, tablero) {
+        const ficha = this.fichas[idFicha];
+        if (ficha.encasa && dado === 5) {
+            ficha.sacarFichas();
+            tablero.colocarFichaEnInicio(ficha);
+        } else if (!ficha.encasa) {
+            tablero.moverFicha(ficha, dado);
         }
-        else{
-           window.print("Selecciona una ficha que no esté en casa para mover")
-           const fichaSeleccionada = await this.esperarClickFicha();
-            if (fichaSeleccionada) {
-                fichaSeleccionada.mover(dado); // Mueve la ficha seleccionada según el dado
-            } else {
-                console.error("No se seleccionó ninguna ficha");
-            }
-        }
+        return ficha;
     }
 }
